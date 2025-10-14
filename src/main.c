c790d50915ecff6f2ee2ca285ca332c383a86b79
jlm22010 <jonahmerriam2@gmail.com>
2025-10-14 14:33:46 -0400

    switched over to arena for long lasting objects in main like source

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "arena.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,13 @@ char *readFile(const char *path, size_t *outLen){
 	FILE *file = fopen(path, "rb");	// rb means read as binary so I get all bytes
 	if(!file)
 		return NULL;
+	
+	// check whether the arena has been initialized
+	if(!arenaIsInitialized()){
+		fprintf(stderr, "[FATAL]: Arena did not initialize.\n");
+
+		exit(10);
+	}
 
 	// get the file size in bytes
 	fseek(file, 0, SEEK_END);
@@ -15,7 +23,7 @@ char *readFile(const char *path, size_t *outLen){
 	fseek(file, 0, SEEK_SET);
 
 	// allocate the char buffer size
-	char *buffer = malloc(sizeof(char) * size + 1);	// add one for EOF
+	char *buffer = arenaAlloc(sizeof(char) * size + 1);	// add one for EOF
 	if(!buffer){
 		fclose(file);
 	
@@ -24,7 +32,6 @@ char *readFile(const char *path, size_t *outLen){
 	
 	// read the file into the buffer
 	if(fread(buffer, 1, size, file) != (size_t)size){
-		free(buffer);
 		fclose(file);
 		
 		return NULL;
@@ -38,20 +45,35 @@ char *readFile(const char *path, size_t *outLen){
 }
 
 int main(int argc, char **argv){
+	// get user input
+	// at this stage it should only be an input file
 	if(argc < 2 || argc >= 3){
-		printf("Usage: a-lang <path/to/file>\n");
+		fprintf(stderr, "Usage: ./a-lang <./path/to/file>\n");
 
 		return 1;
 	}
 
+	// initialize the arena that'll hold the source and long lasting objects
+	arenaInit();
+
+	// initialize variables for getting the source buffer
 	size_t length = 0;
 	char *buffer = NULL;
+
+	// get the source buffer
 	buffer = readFile(argv[argc - 1], &length);
+	if(!buffer){
+		fprintf(stderr, "[FATAL]: Could not load file buffer.\n");
+		arenaDestroy();
+
+		return 2;
+	}
 
 	// just for testing prints the file and it's length
 	printf("%s\n%s contains %ld Bytes\n", buffer, argv[argc - 1], length);
 
-	free(buffer);
+	// cleanup
+	arenaDestroy();
 
 	return 0;
 }
