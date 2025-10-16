010744685d9729a838b08a961a8d59161544205d
jlm22010 <jonahmerriam2@gmail.com>
2025-10-16 13:58:07 -0400

    made linked list library simpler to type out as just list

---
+++ linkedList.c
@@ -0,0 +1,32 @@
+#include "linkedList.h"
+
+size_t listLen(List *list){
+	return list->length;
+}
+
+// queue behavior
+void listEnqueue(List *list, void *ptr){
+	
+}
+
+void *listDequeue(List *list){
+
+}
+
+// stack behavior
+void listPush(List *list, void *ptr){
+
+}
+
+void *listPop(List *list){
+
+}
+
+// list behavior
+void listInsert(List *list, size_t idx, void *ptr){
+
+}
+
+void *listRemove(List *list, size_t idx){
+
+}
--- linkedList.h
+++ linkedList.h
@@ -8,7 +8,7 @@ typedef struct Node{
 	// data is stored as a void* so anything can be stored
 	// might be annoying to manually manage memory
 	// recommend using arena in combo with this for ease of memory use
-	void *ptr
+	void *ptr;
 
 	// forward and backwards links
 	// makes this a doubly linked list
@@ -16,32 +16,32 @@ typedef struct Node{
 	struct Node *next;
 } Node;
 
-typedef struct LinkedList{
+typedef struct List{
 	// Include both head tail pointer for ease of use and speed
 	Node *head;
 	Node *tail;
 
 	// basic length
 	size_t length;
-} LinkedList;
+} List;
 
 // get the length of the list
-size_t linkedListLen(LinkedList *list);
+size_t listLen(List *list);
 
 // queue behavior for linked list
-void linkedListEnqueue(LinkedList *list, void *ptr);
-void *linkedListDequeue(LinkedList *list);
+void listEnqueue(List *list, void *ptr);
+void *listDequeue(List *list);
 
 // stack behavior for linked list
-void linkedListPush(LinkedList *list, void *ptr);
-void *linkedListPop(LinkedList *list);
+void listPush(List *list, void *ptr);
+void *listPop(List *list);
 
 // list behavior for linked list
-void linkedListInsert(LinkedList *list, size_t idx, void *ptr);
-void *linkedListRemove(LinkedList *list, size_t idx);
+void listInsert(List *list, size_t idx, void *ptr);
+void *listRemove(List *list, size_t idx);
 
 // creation and destruction for linked list
-void linkedListInit(LinkedList *list);
-void linkedListDestroy(LinkedList *list);
+void listInit(List *list);
+void listDestroy(List *list);
 
 #endif
